cfb1f9c127c7a178dad9474ab2c49c59871aad25
Javier Martinez <javier0097@hotmail.com>
2024-12-04 13:29:37 -0400

    init commit

---
+++ src/main/java/javier/chest/ChestTestClient.java
@@ -0,0 +1,9 @@
+package javier.chest;
+
+import net.fabricmc.api.ClientModInitializer;
+public class ChestTestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
